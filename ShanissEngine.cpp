d784520e8c574d32305e652cf582ea6a2fb4fd83
Kofu145g <kofu145g@gmail.com>
2021-08-06 17:19:22 -0700

    Initial CMake stuff commit

---
+++ ShanissEngine.cpp
@@ -0,0 +1,12 @@
+﻿// ShanissEngine.cpp : Defines the entry point for the application.
+//
+
+#include "ShanissEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
